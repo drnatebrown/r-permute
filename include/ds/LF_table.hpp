8090c98e48220189bc9a0b772dbb739d2dd959f0
drnatebrown <drnatebrown@gmail.com>
2024-08-08 12:19:34 -0400

    Add LF table

---
+++ include/ds/LF_table.hpp
@@ -0,0 +1,437 @@
+/* Lf_table - Uncompressed version of OptBWTR (LF table)
+    Copyright (C) 2021 Nathaniel Brown
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see http://www.gnu.org/licenses/ .
+*/
+/*!
+   \file LF_table.hpp
+   \brief LF_table.hpp Uncompressed version of OptBWTR (LF table)
+   \author Nathaniel Brown
+   \author Massimiliano Rossi
+   \date 19/11/2021
+*/
+
+#ifndef _LF_TABLE_HH
+#define _LF_TABLE_HH
+
+#include "sdsl/int_vector.hpp"
+#include <algorithm>
+#include <common.hpp>
+
+#include <sdsl/structure_tree.hpp>
+#include <sdsl/util.hpp>
+
+using namespace std;
+
+class LF_table
+{
+public:
+    // Row of the LF table
+    typedef struct LF_row
+    {
+        char character;
+        ulint length;
+        ulint interval;
+        ulint offset;
+
+        size_t serialize(std::ostream &out, sdsl::structure_tree_node *v = nullptr, std::string name ="")
+        {
+            sdsl::structure_tree_node *child = sdsl::structure_tree::add_child(v, name, sdsl::util::class_name(*this));
+            size_t written_bytes = 0;
+
+            out.write((char *)&character, sizeof(character));
+            written_bytes += sizeof(character);
+
+            out.write((char *)&interval, sizeof(interval));
+            written_bytes += sizeof(interval);
+
+            out.write((char *)&length, sizeof(length));
+            written_bytes += sizeof(length);
+
+            out.write((char *)&offset, sizeof(offset));
+            written_bytes += sizeof(offset);
+
+            return written_bytes;
+        }
+
+        void load(std::istream &in)
+        {
+            in.read((char *)&character, sizeof(character));
+            in.read((char *)&interval, sizeof(interval));
+            in.read((char *)&length, sizeof(length));
+            in.read((char *)&offset, sizeof(offset));
+        }
+    };
+
+    LF_table() {}
+
+    LF_table(std::ifstream &heads, std::ifstream &lengths, ulint max_run = 0)
+    {
+        heads.clear();
+        heads.seekg(0);
+        lengths.clear();
+        lengths.seekg(0);
+        
+        LF_runs = vector<LF_row>();
+        vector<vector<size_t>> L_block_indices = vector<vector<size_t>>(ALPHABET_SIZE);
+        
+        char c;
+        ulint i = 0;
+        r = 0;
+        n = 0;
+        while ((c = heads.get()) != EOF)
+        {
+            size_t length = 0;
+            lengths.read((char *)&length, 5);
+            if (c <= TERMINATOR) c = TERMINATOR;
+
+            if (max_run > 0 && length > max_run) {
+                ulint max_splits = length/max_run;
+                for (size_t split = 0; split < max_splits; ++split)
+                {
+                    LF_runs.push_back({c, max_run, 0, 0});
+                    L_block_indices[c].push_back(i++);
+                }
+
+                if (length % max_run != 0)
+                {
+                    LF_runs.push_back({c, length % max_run, 0, 0});
+                    L_block_indices[c].push_back(i++);
+                }
+            }
+            else {
+                LF_runs.push_back({c, length, 0, 0});
+                L_block_indices[c].push_back(i++);
+            }    
+            n+=length;
+        }
+        r = LF_runs.size();
+
+        ulint curr_L_num = 0;
+        ulint L_seen = 0;
+        ulint F_seen = 0;
+        for(size_t i = 0; i < L_block_indices.size(); ++i) 
+        {
+            for(size_t j = 0; j < L_block_indices[i].size(); ++j) 
+            {
+                ulint pos = L_block_indices[i][j];
+
+                LF_runs[pos].interval = curr_L_num;
+                LF_runs[pos].offset = F_seen - L_seen;
+
+                F_seen += LF_runs[pos].length;
+            
+                while (curr_L_num < r && F_seen >= L_seen + LF_runs[curr_L_num].length) 
+                {
+                    L_seen += LF_runs[curr_L_num].length;
+                    ++curr_L_num;
+                }
+            }
+        }
+
+        mem_stats();
+    }
+
+    LF_table(std::ifstream &bwt, ulint max_run = 0)
+    {
+        bwt.clear();
+        bwt.seekg(0);
+        
+        LF_runs = vector<LF_row>();
+        vector<vector<size_t>> L_block_indices = vector<vector<size_t>>(ALPHABET_SIZE);
+        
+        char last_c;
+        char c;
+        ulint i = 0;
+        r = 0;
+        n = 0;
+        size_t length = 0;
+        while ((c = bwt.get()) != EOF)
+        {
+            if (c <= TERMINATOR) c = TERMINATOR;
+            
+            if (i != 0 && c != last_c)
+            {
+                if (max_run > 0 && length > max_run) {
+                    ulint max_splits = length/max_run;
+                    for (size_t split = 0; split < max_splits; ++split)
+                    {
+                        LF_runs.push_back({last_c, max_run, 0, 0});
+                        L_block_indices[last_c].push_back(i++);
+                    }
+
+                    if (length % max_run != 0)
+                    {
+                        LF_runs.push_back({last_c, length % max_run, 0, 0});
+                        L_block_indices[last_c].push_back(i++);
+                    }
+                }
+                else {
+                    LF_runs.push_back({last_c, length, 0, 0});
+                    L_block_indices[last_c].push_back(i++);
+                }    
+                n+=length;
+                length = 0;
+            }
+            ++length;
+            last_c = c;
+        }
+        // Step for final character
+        if (max_run > 0 && length > max_run) {
+            ulint max_splits = length/max_run;
+            for (size_t split = 0; split < max_splits; ++split)
+            {
+                LF_runs.push_back({last_c, max_run, 0, 0});
+                L_block_indices[last_c].push_back(i++);
+            }
+
+            if (length % max_run != 0)
+            {
+                LF_runs.push_back({last_c, length % max_run, 0, 0});
+                L_block_indices[last_c].push_back(i++);
+            }
+        }
+        else {
+            LF_runs.push_back({last_c, length, 0, 0});
+            L_block_indices[last_c].push_back(i++);
+        }    
+        n+=length;
+
+        r = LF_runs.size();
+
+        ulint curr_L_num = 0;
+        ulint L_seen = 0;
+        ulint F_seen = 0;
+        for(size_t i = 0; i < L_block_indices.size(); ++i) 
+        {
+            for(size_t j = 0; j < L_block_indices[i].size(); ++j) 
+            {
+                ulint pos = L_block_indices[i][j];
+
+                LF_runs[pos].interval = curr_L_num;
+                LF_runs[pos].offset = F_seen - L_seen;
+
+                F_seen += LF_runs[pos].length;
+            
+                while (curr_L_num < r && F_seen >= L_seen + LF_runs[curr_L_num].length) 
+                {
+                    L_seen += LF_runs[curr_L_num].length;
+                    ++curr_L_num;
+                }
+            }
+        }
+
+        mem_stats();
+    }
+
+    LF_table(std::ifstream &heads, std::ifstream &lengths, sdsl::bit_vector splits)
+    {
+        heads.clear();
+        heads.seekg(0);
+        lengths.clear();
+        lengths.seekg(0);
+
+        LF_runs = vector<LF_row>();
+        vector<vector<size_t>> L_block_indices = vector<vector<size_t>>(ALPHABET_SIZE);
+
+        char c;
+        ulint i = 0;
+        r = 0;
+        ulint bv_r = 0;
+        ulint true_r = 0;
+        for (size_t i = 0; i < splits.size(); ++i) {
+            bv_r += splits[i];
+        }
+        n = 0;
+        while ((c = heads.get()) != EOF)
+        {
+            size_t length = 0;
+            lengths.read((char *)&length, 5);
+            n+=length;
+            true_r += 1;
+            if (c <= TERMINATOR) c = TERMINATOR;
+
+            size_t curr_len = 1; // Assume we start at a run-head
+            for (size_t bwt_i = n - length + 1; bwt_i < n; bwt_i++)
+            {
+                if (splits[bwt_i]) 
+                {
+                    LF_runs.push_back({c, curr_len, 0, 0});
+                    L_block_indices[c].push_back(i++);
+                    curr_len = 0;
+                }
+                curr_len++;
+            }
+            LF_runs.push_back({c, curr_len, 0, 0});
+            L_block_indices[c].push_back(i++);
+
+        }
+        r = LF_runs.size();
+
+        ulint curr_L_num = 0;
+        ulint L_seen = 0;
+        ulint F_seen = 0;
+        for(size_t i = 0; i < L_block_indices.size(); ++i) 
+        {
+            for(size_t j = 0; j < L_block_indices[i].size(); ++j) 
+            {
+                ulint pos = L_block_indices[i][j];
+
+                LF_runs[pos].interval = curr_L_num;
+                LF_runs[pos].offset = F_seen - L_seen;
+
+                F_seen += LF_runs[pos].length;
+
+                while (curr_L_num < r && F_seen >= L_seen + LF_runs[curr_L_num].length) 
+                {
+                    L_seen += LF_runs[curr_L_num].length;
+                    ++curr_L_num;
+                }
+            }
+        }
+
+        cout << "True r: " << true_r << endl;
+        cout << "BV r: " << bv_r << endl;
+        mem_stats();
+    }
+
+    const LF_row get(size_t i)
+    {
+        assert(i < LF_runs.size());
+        return LF_runs[i];
+    }
+
+    ulint size()
+    {
+        return n;
+    }
+
+    ulint runs()
+    {
+        return r;
+    }
+
+    void invert(std::string outfile) 
+    {
+        std::ofstream out(outfile);
+
+        ulint interval = 0;
+        ulint offset = 0;
+
+        char c;
+        while((c = get_char(interval)) > TERMINATOR) 
+        {
+            out << c;
+            std::pair<ulint, ulint> pos = LF(interval, offset);
+            interval = pos.first;
+            offset = pos.second;
+        }
+    }
+
+    /*
+     * \param Run position (RLE intervals)
+     * \param Current character offset in block
+     * \return block position and offset of preceding character
+     */
+    std::pair<ulint, ulint> LF(ulint run, ulint offset)
+    {
+        ulint next_interval = LF_runs[run].interval;
+        ulint next_offset = LF_runs[run].offset + offset;
+
+        while (next_offset >= LF_runs[next_interval].length) 
+        {
+            next_offset -= LF_runs[next_interval++].length;
+        }
+
+        return std::make_pair(next_interval, next_offset);
+    }
+
+    uchar get_char(ulint i)
+    {
+        return get(i).character;
+    }
+
+    std::string get_file_extension() const
+    {
+        return ".LF_table";
+    }
+
+    void mem_stats()
+    {
+        sdsl::nullstream ns;
+
+        verbose("Memory consumption (bytes).");
+        verbose("              LF table: ", serialize(ns));
+    }
+
+    void bwt_stats()
+    {
+        ulint n = size();
+        ulint r = runs();
+        verbose("Number of BWT equal-letter runs: r = ", r);
+        verbose("Length of complete BWT: n = ", n);
+        verbose("Rate n/r = ", double(n) / r);
+        verbose("log2(r) = ", log2(double(r)));
+        verbose("log2(n/r) = ", log2(double(n) / r));
+    }
+
+    /* serialize to the ostream
+    * \param out     the ostream
+    */
+    size_t serialize(std::ostream &out, sdsl::structure_tree_node *v = nullptr, std::string name ="")
+    {
+        sdsl::structure_tree_node *child = sdsl::structure_tree::add_child(v, name, sdsl::util::class_name(*this));
+        size_t written_bytes = 0;
+
+        out.write((char *)&n, sizeof(n));
+        written_bytes += sizeof(n);
+
+        out.write((char *)&r, sizeof(r));
+        written_bytes += sizeof(r);
+
+        size_t size = LF_runs.size();
+        out.write((char *)&size, sizeof(size));
+        written_bytes += sizeof(size);
+
+        for(size_t i = 0; i < size; ++i)
+        {
+            written_bytes += LF_runs[i].serialize(out, v, "LF_run_" + std::to_string(i));
+        }
+
+        return written_bytes;
+    }
+
+    /* load from the istream
+    * \param in the istream
+    */
+    void load(std::istream &in)
+    {
+        size_t size;
+
+        in.read((char *)&n, sizeof(n));
+        in.read((char *)&r, sizeof(r));
+
+        in.read((char *)&size, sizeof(size));
+        LF_runs = std::vector<LF_row>(size);
+        for(size_t i = 0; i < size; ++i)
+        {
+            LF_runs[i].load(in);
+        }
+    }
+
+private:
+    ulint n; // Length of BWT
+    ulint r; // Runs of BWT
+
+    vector<LF_row> LF_runs;
+};
+
+#endif /* end of include guard: _LF_TABLE_HH */
---
+++ test/src/build_permute.cpp
@@ -0,0 +1,81 @@
+/* build_permute - Build the simple R-Index-F table with splitting
+    Copyright (C) 2021 Nathaniel Brown
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see http://www.gnu.org/licenses/ .
+*/
+/*!
+   \file build_permute.cpp
+   \brief build_permute.cpp Build the simple R-Index-F table with splitting
+   \author Nathaniel Brown
+   \date 02/11/2021
+*/
+
+#define VERBOSE
+
+#include "LF_table.hpp"
+#include <common.hpp>
+#include <sdsl/io.hpp>
+#include <sdsl/int_vector.hpp>
+#include <malloc_count.h>
+
+
+int main(int argc, char *const argv[])
+{
+  Args args;
+  parseArgs(argc, argv, args);
+
+  // Building the r-index-f table
+
+  verbose("Building the Permute Table for LF");
+  std::chrono::high_resolution_clock::time_point t_insert_start = std::chrono::high_resolution_clock::now();
+
+  std::string bwt_fname = args.filename + ".bwt";
+  std::string bwt_heads_fname = bwt_fname + ".heads";
+  std::ifstream ifs_heads(bwt_heads_fname);
+  std::string bwt_len_fname = bwt_fname + ".len";
+  std::ifstream ifs_len(bwt_len_fname);
+
+  LF_table rif;
+
+  if (args.d) {
+    std::string splitting_filename = args.filename + ".d_col";
+    std::ifstream ifs_split(splitting_filename);
+    sdsl::bit_vector run_splits;
+    run_splits.load(ifs_split);
+
+    rif = LF_table(ifs_heads, ifs_len, run_splits);
+  }
+  else {
+    rif = LF_table(ifs_heads, ifs_len);
+  }
+
+  std::chrono::high_resolution_clock::time_point t_insert_end = std::chrono::high_resolution_clock::now();
+
+  rif.mem_stats();
+  rif.bwt_stats();
+
+  verbose("Construction Complete");
+  verbose("Memory peak: ", malloc_count_peak());
+  verbose("Elapsed time (s): ", std::chrono::duration<double, std::ratio<1>>(t_insert_end - t_insert_start).count());
+
+  verbose("Serializing Table");
+
+  std::string outfile = args.filename + rif.get_file_extension();
+  std::ofstream out(outfile);
+  rif.serialize(out);
+  out.close();
+  t_insert_end = std::chrono::high_resolution_clock::now();
+
+  verbose("Memory peak: ", malloc_count_peak());
+  verbose("Elapsed time (s): ", std::chrono::duration<double, std::ratio<1>>(t_insert_end - t_insert_start).count());
+
+  return 0;
+}
